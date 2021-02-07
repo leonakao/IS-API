1c4500fead98ecb9c55533b42dc5d56587cbc991
Leonardo Nakao <leonakao.dev@gmail.com>
2021-02-06 23:51:46 -0300

    create route to list all members

--- src/routes/member.routes.ts
+++ src/routes/member.routes.ts
@@ -1,9 +1,12 @@
+import Member, { MemberDocument } from '@models/Member';
 import { Router } from 'express'
 
 const memberRoutes = Router()
 
 memberRoutes.get('/', async (req, res) => {
-  return res.status(200).json([]);
+  const members = await Member.find();
+
+  return res.status(200).json(members);
 })
 
 export default memberRoutes
